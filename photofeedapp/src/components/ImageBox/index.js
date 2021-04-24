5f8a8be7b47815d44305419d87a6f4f13a82871d
Kshitinjay <kshitinjay20@gmail.com>
2021-04-24 10:45:43 +0530

    last commit

--- photofeedapp/src/components/ImageBox/index.js
+++ photofeedapp/src/components/ImageBox/index.js
@@ -3,7 +3,6 @@ import "./imgbox.css";
 const ImageBox = ({ image, handleSwitch }) => {
   return (
     <div className="actualImg">
-      <h1>ImageBox</h1>
       <img src={image} alt="noImage" id="myImg" onClick={handleSwitch} />
     </div>
   );
